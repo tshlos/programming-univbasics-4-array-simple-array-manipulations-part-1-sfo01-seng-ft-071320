2457b1ae641c19e6adf2b9174e169ade582817e3
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-13 22:27:41 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array, string) 
+  array.push(string)
+  array
+end
+
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+
+def using_pop(array)
+  array.pop()
+end
+
+
+def pop_with_args(array)
+  removed = array.pop(2)
+  removed
+end
+
+
+def using_shift(array)
+  array.shift()
+end
+
+
+def shift_with_args(array)
+  removed = array.shift(2)
+  removed
+end
